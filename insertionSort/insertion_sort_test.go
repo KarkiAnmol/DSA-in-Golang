bdceb8d35fed5dc2b0ef25c93beee4213cc50da3
Anmol Karki <anmolkarki101@gmail.com>
2024-04-24 20:44:38 +0545

    feat: Print informative messages for each test result in TestInsertionSort

--- insertionSort/insertion_sort_test.go
+++ insertionSort/insertion_sort_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -19,11 +20,13 @@ func TestInsertionSort(t *testing.T) {
 		{"random order", []int{3, 1, 4, 1, 5}, []int{1, 1, 3, 4, 5}},
 	}
 
-	for _, tt := range tests {
+	for i, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := InsertionSort(tt.input)
 			if !reflect.DeepEqual(got, tt.expected) {
 				t.Errorf("InsertionSort() = %v, want %v", got, tt.expected)
+			} else {
+				fmt.Printf("Test case %d '%s'.............PASSED ✅\n", i, tt.name)
 			}
 		})
 	}
